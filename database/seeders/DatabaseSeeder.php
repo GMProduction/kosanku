552290ace393dac5439bfa94fc00f3230fe2efe4
portgazdbeel <portgaz77@gmail.com>
2021-11-19 12:28:04 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'nama' => 'Admin',
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'roles' => 'admin',
+            'alamat' => 'Solo',
+            'no_hp' => '11234511515',
+        ]);
     }
 }
